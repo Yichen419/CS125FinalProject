270cd2eb9372094ee572d544a91ef286aa42dbc7
yichenliu <yliu290@illinois.edu>
2018-12-05 15:58:25 -0600

    First Try

--- app/src/main/java/com/example/yichenliu/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/yichenliu/cs125finalproject/MainActivity.java
@@ -13,14 +13,26 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static ImageView imgview;
-    private int currentImage;
-    int[] images = {R.drawable.asianfood2, R.drawable.asianfood3, R.drawable.asianfood3,
-            R.drawable.asianfood4, R.drawable.bbq1, R.drawable.bbq2, R.drawable.bbq3,
-            R.drawable.bbq4, R.drawable.burger1, R.drawable.burger2, R.drawable.burger3,
-            R.drawable.burger4, R.drawable.italianfood1, R.drawable.italianfood2,
-            R.drawable.italianfood3, R.drawable.italianfood4, R.drawable.mexicanfood1,
-            R.drawable.mexicanfood2, R.drawable.mexicanfood3,R.drawable.mexicanfood4,
-            R.drawable.seafood1, R.drawable.seafood2, R.drawable.seafood4};
+    private int currentImage = -1;
+    private int asianFoodCount;
+    private int bbqCount;
+    private int burgerCount;
+    private int italianCount;
+    private int mexicanCount;
+    private int seafoodCount;
+    private int veggieCount;
+
+    int[] images = {
+            R.drawable.asianfood1, R.drawable.bbq1, R.drawable.burger1,
+            R.drawable.italianfood1, R.drawable.mexicanfood1, R.drawable.seafood1, R.drawable.veggie1,
+            R.drawable.asianfood2, R.drawable.bbq2, R.drawable.burger2,
+            R.drawable.italianfood2, R.drawable.mexicanfood2, R.drawable.seafood2, R.drawable.veggie2,
+            R.drawable.asianfood3, R.drawable.bbq3, R.drawable.burger3,
+            R.drawable.italianfood3, R.drawable.mexicanfood3, R.drawable.seafood3, R.drawable.veggie3,
+            R.drawable.asianfood4, R.drawable.bbq4, R.drawable.burger4,
+            R.drawable.italianfood4, R.drawable.mexicanfood4, R.drawable.seafood4, R.drawable.veggie4,
+
+    };
     int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +50,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 imgview = (ImageView)findViewById(R.id.imageView);
+                if (currentImage % 7 == 0) {
+                    asianFoodCount++;
+                } else if (currentImage % 7 == 1) {
+                    bbqCount++;
+                } else if (currentImage % 7 == 2) {
+                    burgerCount++;
+                } else if (currentImage % 7 == 3) {
+                    italianCount++;
+                } else if (currentImage % 7 == 4) {
+                    mexicanCount++;
+                } else if (currentImage % 7 == 5) {
+                    seafoodCount++;
+                } else if (currentImage % 7 == 6) {
+                    veggieCount++;
+                }
                 currentImage++;
                 currentImage = currentImage % images.length;
                 imgview.setImageResource(images[currentImage]);
+                counter++;
             }
         });
         btnDislike.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 currentImage++;
                 currentImage = currentImage % images.length;
                 imgview.setImageResource(images[currentImage]);
+                counter++;
             }
         });
         btnDecide.setOnClickListener(new View.OnClickListener() {
